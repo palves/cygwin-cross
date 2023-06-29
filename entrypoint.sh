075487e67048742095e5d1f2227d37c320fbf74c
Pedro Alves <pedro@palves.net>
2023-06-29 13:28:01 +0100

    Don't run passed commands if LOCAL_CWD isn't mounted in container
    This prevents something like:
    
      cd /path/not/mounted/in/container/
      cygwin-cross rm -rf
    
    ... from running the command on the wrong directory.
    
    Noticed by inspection, thankfully.
    
    Do the same for the plain enter-container case too, for consistency.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ groupadd $LOCAL_GROUP -g $LOCAL_GID
 useradd -d $HOME -s /bin/bash -M $USER -u $LOCAL_UID -g $LOCAL_GID
 
 if [ $# -gt 0 ]; then
-    exec su-exec ${USER} sh -c "cd $LOCAL_CWD; $*"
+    exec su-exec ${USER} sh -c "cd $LOCAL_CWD && $*"
 else
-    exec su-exec ${USER} sh -c "cd $LOCAL_CWD; /bin/bash --login"
+    exec su-exec ${USER} sh -c "cd $LOCAL_CWD && /bin/bash --login"
 fi
